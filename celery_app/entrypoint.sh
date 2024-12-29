91fcb588b8a553b26e4aeeec87f7c1baa393676b
shivanshu <shivanshu1333@gmail.com>
2024-12-30 00:29:18 +0530

    nit: add changes

--- celery_app/entrypoint.sh
+++ celery_app/entrypoint.sh
@@ -4,8 +4,8 @@
 echo "Performing pre-execution tasks..."
 
 # Introduce a delay (e.g., 10 seconds)
-echo "Waiting for 10 seconds before starting the Celery worker..."
-sleep 180
+echo "Waiting for 60 seconds before starting the Celery worker starts..."
+sleep 60
 
 # Execute the main command passed as arguments to the script
 exec "$@"
