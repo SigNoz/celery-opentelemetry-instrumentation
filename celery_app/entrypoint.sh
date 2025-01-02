281d542697b82a1986a72b8631c6027d91eb9834
Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>
2025-01-02 23:13:04 +0530

    chore: add updates
    Signed-off-by: Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>

--- celery_app/entrypoint.sh
+++ celery_app/entrypoint.sh
@@ -5,7 +5,7 @@ echo "Performing pre-execution tasks..."
 
 # Introduce a delay (e.g., 10 seconds)
 echo "Waiting for 60 seconds before starting the Celery worker starts..."
-sleep 60
+sleep 120
 
 # Execute the main command passed as arguments to the script
 exec "$@"
